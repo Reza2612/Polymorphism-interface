201efe4114f76333615552704a7d69c5e6d37495
Reza2612 <61917869+Reza2612@users.noreply.github.com>
2020-05-13 20:29:11 +0700

    Add files via upload

---
+++ PrinterWindows.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism_interface
+{
+
+    interface IPrinterWindows
+    {
+        void tmpl();
+        void Print();
+    }
+}
